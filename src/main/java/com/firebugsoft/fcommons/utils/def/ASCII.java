f04a22b811239cb880f9ce93ca192f0011b2767b
felix <felix@localhost>
2014-12-17 22:22:38 +0800

    tmp

---
+++ src/main/java/com/firebugsoft/fcommons/utils/def/ASCII.java
@@ -0,0 +1,14 @@
+package com.firebugsoft.fcommons.utils.def;
+
+public final class ASCII {
+    private ASCII() {}
+
+    /** ascii码: 换行 */
+    public static final byte LF          = (byte) 0x0A;
+    /** ascii码: 回车 */
+    public static final byte CR          = (byte) 0x0D;
+    /** ascii码: 空格 */
+    public static final byte SPACE       = (byte) 0x20;
+    /** ascii码: _ */
+    public static final byte UNDER_SCORE = (byte) 0x5F;
+}
